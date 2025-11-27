6f571011ced75dea10e40f1fb93ecca5347cd146
jackson <jacksonvargas134@gmail.com>
2025-11-27 11:49:59 -0300

    adição imagem hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,28 +1,43 @@
 import { Calendar, Phone } from 'lucide-react'
+import heroImage from '../assets/heroImage.png'
+import Image from 'next/image'
 
 export default function Hero() {
-  return(
+  return (
     <>
-      <section className="min-h-screen flex flex-col justify-center gap-3 px-5 md:px-20">
-        <h3 className='text-black text-[5rem] max-sm:text-[2.8rem] max-sm:leading-13 font-semibold leading-18'>
-          Seu Sorriso
+      <section className="relative min-h-screen flex flex-col justify-center gap-3 px-5 md:px-20">
 
-          <span className='block text-teal-500'>Merece o Melhor</span>
-        </h3>
-        <p className='text-[1.2rem] text-zinc-500 max-w-[600px] mb-5'>
-          Tecnologia de ponta e atendimento humanizado para cuidar da sua saúde bucal com excelência.
-        </p>
+        <div className="absolute inset-0 z-0">
+          <Image
+            src={heroImage}
+            alt='Imagem hero'
+            className='w-full h-full object-cover'
+          />
 
-        <div className='flex flex-wrap items-center gap-2'>
-          <a href="#" className='flex items-center gap-3 py-3 px-5 text-[1.1rem] bg-teal-500 text-white rounded-full hover:bg-teal-400'>
-            <Calendar size={18} />
-            Agende sua Consulta
-          </a>
+          <div className="absolute inset-0 bg-linear-to-r from-black/95 via-black/80 to-black/80" />
+        </div>
 
-          <a href="#" className='flex items-center gap-3 py-3 px-5 text-[1.1rem] bg-teal-500 text-white rounded-full hover:bg-teal-400'>
-            <Phone size={18} />
-            (55) 9999-9999
-          </a>
+        <div className='relative'>
+          <h3 className='text-white text-[5rem] max-sm:text-[2.8rem] max-sm:leading-13 font-semibold leading-18'>
+            Seu Sorriso
+
+            <span className='block text-teal-500 mb-2'>Merece o Melhor</span>
+          </h3>
+          <p className='text-[1.2rem] text-zinc-300 max-w-[600px] mb-8'>
+            Tecnologia de ponta e atendimento humanizado para cuidar da sua saúde bucal com excelência.
+          </p>
+
+          <div className='flex flex-wrap items-center gap-2'>
+            <a href="#" className='flex items-center gap-3 py-3 px-5 text-[1.1rem] bg-white text-teal-600 rounded-full transition-all duration-300 hover:-translate-y-1 animate-scale-in'>
+              <Calendar size={18} />
+              Agende sua Consulta
+            </a>
+
+            <a href="#" className='flex items-center gap-3 py-3 px-5 text-[1.1rem] bg-white text-teal-600 rounded-full transition-all duration-300 hover:-translate-y-1 animate-scale-in'>
+              <Phone size={18} />
+              (55) 9999-9999
+            </a>
+          </div>
         </div>
       </section>
     </>
